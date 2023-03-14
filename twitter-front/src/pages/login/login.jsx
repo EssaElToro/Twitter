08646fe8e8a65f749965ebd3814989a84a854320
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-03-14 13:49:35 +0100

    edit login app

--- twitter-front/src/pages/login/login.jsx
+++ twitter-front/src/pages/login/login.jsx
@@ -1,10 +1,23 @@
-import "./Login.scss";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Twitter</h1>
-    </div>
+    <>
+      <div className="header">
+        <h1>Twitter</h1>
+      </div>
+      <div>
+        <form>
+          <label>Login : </label>
+          <input type="text"></input>
+          <br></br>
+          <label>Password : </label>
+          <input type="text"></input>
+          <br></br>
+        </form>
+        <input type="submit" value="login"></input>
+      </div>
+    </>
   );
 };
 
