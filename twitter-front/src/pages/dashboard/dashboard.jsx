a096d0abb7c88f92b1975edd2d9acfd6adb82731
DKK123_ <u50_kaczeb_waw@technischools.com>
2023-03-15 10:28:38 +0100

    Dashboard

---
+++ twitter-front/src/pages/dashboard/dashboard.jsx
@@ -0,0 +1,21 @@
+import "./dashboard.scss";
+
+// Make a dashboard for the user to see their tweets and other users' tweets
+const Dashboard = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>Twitter</h1>
+      </div>
+
+      <div className="dashboard">
+        <div className="tweets">
+          <h2>Tweets</h2>
+        </div>
+        <div className="users">
+          <h2>Users</h2>
+        </div>
+      </div>
+    </>
+  );
+};
