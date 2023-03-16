bad236a12a566756e9ba3778bfe455e43a1cea3e
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-03-16 11:31:29 +0100

    Update App.js

--- twitter-front/src/App.js
+++ twitter-front/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./pages";
+import { Login, Dashboard } from "./pages";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
