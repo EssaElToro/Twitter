52129716a49b2b2d2bd4dd734f45eb3348afd70f
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-03-16 10:53:32 +0100

    no message

--- twitter-front/src/pages/login/login.jsx
+++ twitter-front/src/pages/login/login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   return (
     <>
       <div className="header">
-        <h1>Twitter</h1>
+        <h1>Twitter1</h1>
       </div>
       <div className="box">
         <form onSubmit={loginHandler}>
