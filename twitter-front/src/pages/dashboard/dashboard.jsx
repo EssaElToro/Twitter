9d88f1622f5bf3b432bbcd08512886756853e50a
DKK123_ <u50_kaczeb_waw@technischools.com>
2023-03-16 11:10:32 +0100

    add register, uprade dashboard

--- twitter-front/src/pages/dashboard/dashboard.jsx
+++ twitter-front/src/pages/dashboard/dashboard.jsx
@@ -1,21 +1,32 @@
 import "./dashboard.scss";
 
-// Make a dashboard for the user to see their tweets and other users' tweets
+// Make a dashboard for the user to see and add their tweets like and comment other users tweets
+
 const Dashboard = () => {
   return (
     <>
       <div className="header">
         <h1>Twitter</h1>
       </div>
-
-      <div className="dashboard">
-        <div className="tweets">
-          <h2>Tweets</h2>
-        </div>
-        <div className="users">
-          <h2>Users</h2>
-        </div>
+      <div className="box">
+        <form>
+          <label>Tweet : </label>
+          <input type="text"></input>
+          <br></br>
+          <label>Comment : </label>
+          <input type="text"></input>
+          <br></br>
+        </form>
+        <a href="#">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Tweet
+        </a>
       </div>
     </>
   );
 };
+
+export default Dashboard;
