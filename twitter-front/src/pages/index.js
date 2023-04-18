adc41302ebc9cbcb61a94e0a16f918d32f003bc6
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-04-18 16:24:01 +0200

    Update index.js

--- twitter-front/src/pages/index.js
+++ twitter-front/src/pages/index.js
@@ -2,5 +2,7 @@ import Dashboard from "./dashboard/dashboard";
 import Login from "./login/login";
 import Register from "./register/register";
 import Email from "./email/email";
+import Reminder from "./reminder/reminder";
+import Password from "./password/password";
 
-export { Dashboard, Login, Register, Email };
+export { Dashboard, Login, Register, Email, Reminder, Password };
