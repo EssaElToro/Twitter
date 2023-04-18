0d46a63086b9d5bee226cc9ff6b15d7aa03117af
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-04-18 13:54:06 +0200

    Update register.jsx

--- twitter-front/src/pages/register/register.jsx
+++ twitter-front/src/pages/register/register.jsx
@@ -1,63 +1,71 @@
-import React, { useState } from "react";
-import joy from "joy";
+import "./register.scss";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 
-function RegisterModule() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+const Register = () => {
+  const navigate = useNavigate();
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Define the Joy schema for email and password validation
-    const schema = {
-      email: joy.email(),
-      password: joy.string().min(8).max(30),
-    };
-
-    // Validate the inputs against the schema
-    try {
-      joy.validate(schema, { email, password });
-    } catch (e) {
-      setErrorMessage(`Validation error: ${e.message}`);
-      return;
+  const validate = (email, Nickname, password, confirmPassword) => {
+    console.log(email);
+    console.log(email.split(""));
+    console.log(email.split("").includes("@"));
+    if (!email.split("").includes("@")) {
+      swal("Error", "Invalid email address", "error");
+      return false;
     }
+    if (Nickname.length <= 3) {
+      swal("Error", "Nickname must be longer than 3 characters", "error");
+      return false;
+    }
+    if (password !== confirmPassword) {
+      swal("Error", "Passwords do not match", "error");
+      return false;
+    }
+    return true;
+  };
+  const registerHandler = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const Nickname = e.target.uname.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
 
-    // If the validation passes, create a new user account
-    // (replace this with your actual implementation logic)
-    console.log(
-      `Creating new account with email: ${email} and password: ${password}`
-    );
+    if (validate(email, Nickname, password, confirmPassword)) {
+      navigate("/register/email");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-      </label>
-      <br />
-      <button type="submit">Register</button>
-      {errorMessage && <div>{errorMessage}</div>}
-    </form>
+    <>
+      <div className="header">
+        <h1>Twitter</h1>
+      </div>
+      <div className="boxr">
+        <form onSubmit={registerHandler}>
+          <label>Email : </label>
+          <input type="text" name="email" required></input>
+          <br></br>
+          <label>Nickname : </label>
+          <input type="text" name="uname" required></input>
+          <br></br>
+          <label>Password : </label>
+          <input type="password" name="password" required></input>
+          <br></br>
+          <label>Confirm password : </label>
+          <input type="password" name="confirmPassword" required></input>
+          <br></br>
+          <button type="submit" className="submit-button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign in
+          </button>
+          <a href="/login">Already have an account?</a>
+        </form>
+      </div>
+    </>
   );
-}
-
-export default RegisterModule;
+};
+export default Register;
