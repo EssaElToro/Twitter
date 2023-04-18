8a5d45ab3a8ea0860a12ecb041576c0fecea4939
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-04-18 13:52:19 +0200

    edit exports

--- twitter-front/src/pages/index.js
+++ twitter-front/src/pages/index.js
@@ -1,4 +1,6 @@
 import Dashboard from "./dashboard/dashboard";
 import Login from "./login/login";
+import Register from "./register/register";
+import Email from "./email/email";
 
-export { Dashboard, Login };
+export { Dashboard, Login, Register, Email };
