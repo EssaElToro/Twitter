9e572d384062a8a4fb81d9dbf14f708592340538
Igor Niewiadomski <96412905+EssaElToro@users.noreply.github.com>
2023-04-18 13:51:12 +0200

    add email verfication on front

---
+++ twitter-front/src/pages/email/email.jsx
@@ -0,0 +1,41 @@
+import "./email.scss";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Email = () => {
+  const navigate = useNavigate();
+
+  const verify_handler = (e) => {
+    e.preventDefault();
+    navigate("/login ");
+  };
+  return (
+    <>
+      <div className="header">
+        <h1>Twitter</h1>
+      </div>
+      <div className="box">
+        <form onSubmit={verify_handler}>
+          <div className="box-header">
+            <h2>Email verfication</h2>
+            <p>
+              To verify email, please enter your inbox and enter your
+              verfication code{" "}
+            </p>
+          </div>
+          <label>verfication code : </label>
+          <input type="text" name="email" required />
+          <button type="submit" className="submit-button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Email;
