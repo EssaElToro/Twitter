d0ef94419e39d335f50df01c6550dad775d3e9d1
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-05-16 16:32:22 +0200

    updated

--- server/users/service/users.js
+++ server/users/service/users.js
@@ -1,41 +1,16 @@
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+const mongoose = require("mongoose");
 
 const User = require("../../models/users");
 
 const usersAdd = async (req, res) => {
-  const emailRef = React.createRef();
-  const passwordRef = React.createRef();
-
-  const navigate = useNavigate();
-
-  const loginHandler = async (e) => {
-    e.preventDefault();
-
-    const data = {
-      email: emailRef.current.value,
-      password: passwordRef.current.value,
-    };
-
-    const response = await axios.post("/api/users/add", data);
-
-    if (response.data.success) {
-      sessionStorage.setItem("token", response.data.token);
-      navigate("/dashboard");
-    }
-  };
-
+  const data = req.body;
+  const user = new User(data);
   try {
-    const admin = await Admin.findOne({ name: data.login });
-
-    console.log(admin);
-
-    if (!admin) return res.status(200).json({ success: false });
-
-    return res.status(200).json({ success: true });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ success: false });
+    await user.save();
+    res.status(201).json({ success: true, data: user });
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ success: false, message: err.message });
   }
 };
 
--- twitter-front/src/pages/dashboard/dashboard.jsx
+++ twitter-front/src/pages/dashboard/dashboard.jsx
@@ -1,9 +1,30 @@
 import "./dashboard.scss";
 import React from "react";
 import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import ReactModal from "react-modal";
+import { useEffect, useState } from "react";
+import * as icons from "lucide-react";
 
 const Dashboard = () => {
-  const createPost = () => {};
+  const [isOpen, setIsOpen] = useState(false);
+  const styles = {
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.5)",
+    },
+    content: {
+      backgroundColor: "white",
+      width: "90%",
+    },
+  };
+  const [file, setFile] = useState();
+
+  const changeHandler = (e) => {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const twittsHandler = () => {};
   return (
     <>
       <div className="header">
@@ -11,7 +32,33 @@ const Dashboard = () => {
       </div>
       <div className="board">
         <h1>New tweets</h1>
-        <button onClick={createPost}>Create post</button>
+        <div className="popup">
+          <button onClick={() => setIsOpen(true)}>Add tweet</button>
+          <ReactModal
+            isOpen={isOpen}
+            style={styles}
+            contentLabel="Example Modal"
+            onRequestClose={() => setIsOpen(false)}
+          >
+            <div className="post">
+              <icon onClick={() => setIsOpen(false)}>
+                <icons.LucideXSquare></icons.LucideXSquare>
+              </icon>
+              <h1>New tweet</h1>
+              <form onSubmit={twittsHandler}>
+                <h2>Add Image:</h2>
+                <img src={file} />
+                <input type="file" onChange={changeHandler} />
+                <input type="text" placeholder="Title" required />
+                <input type="text" placeholder="Description" />
+                <br></br>
+                <icon type="submit" className="abc">
+                  <icons.LucideSend></icons.LucideSend>
+                </icon>
+              </form>
+            </div>
+          </ReactModal>
+        </div>
       </div>
       <div className="profile">
         <h1>Settings</h1>
