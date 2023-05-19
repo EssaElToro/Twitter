9a2ae5af6179ccb9f03a65b098fbd72a94f6866c
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-05-19 15:52:28 +0200

    add profile

--- twitter-front/src/pages/profile/profile.jsx
+++ twitter-front/src/pages/profile/profile.jsx
@@ -1,4 +1,20 @@
 import "./profile.scss";
+import "./userPhoto.jpg";
 
-const Profile = () => {};
+const Profile = () => {
+  return (
+    <div className="profile">
+      <div className="profile_content">
+        <h1>Profile</h1>
+        <img src="./userPhoto.jpg   " alt="userPhoto" />
+        <h1>Profile Picture</h1>
+        <h1>
+          Username:
+          <br></br>
+          KontoTV
+        </h1>
+      </div>
+    </div>
+  );
+};
 export default Profile;
